9b448194919d5a02d844097e31d2ed58f1cff0b3
PitoyoAidropHunter <pitoyoaidrophunter@gmail.com>
2026-02-08 01:52:20 +0700

    update : home-page

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,11 +1,27 @@
+import AboutPage from "./AboutPage";
+import BenefitPage from "./BenefitPage";
+import ContactPage from "./ContactPage";
+import PortofolioPage from "./PortofolioPage";
 import WelcomePage from "./WelcomePage";
 
 const Home = () => {
   return (
     <>
-      <div>
+      <div className="bg-gray-50 pt-20">
         <section>
           <WelcomePage />
+        </section>
+        <section>
+          <BenefitPage />
+        </section>
+        <section>
+          <AboutPage />
+        </section>
+        <section>
+          <PortofolioPage />
+        </section>
+        <section>
+          <ContactPage />
         </section>
       </div>
     </>
