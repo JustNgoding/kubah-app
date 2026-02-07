8c43ecae021bd9a95c0aa91d79b77aa5845d8d1f
PitoyoAidropHunter <pitoyoaidrophunter@gmail.com>
2026-02-08 01:51:27 +0700

    add new page: about

---
+++ src/components/Home/AboutPage.tsx
@@ -0,0 +1,46 @@
+const AboutPage = () => {
+  return (
+    <div className="p-8 md:p-16 bg-secondary ">
+      <div className="max-w-7xl mx-auto  p-8 md:p-12">
+        <h2 className="text-2xl md:text-3xl font-bold text-gray-800 mb-6">
+          Apa itu Dome Indonesia?
+        </h2>
+
+        <p className="text-gray-700 leading-relaxed mb-4">
+          Dome Indonesia adalah perusahaan yang berfokus pada desain, produksi,
+          dan pemasangan kubah masjid dengan kualitas terbaik. Kami percaya
+          bahwa setiap kubah bukan sekadar elemen arsitektur, tetapi juga simbol
+          kemegahan, keindahan, dan spiritualitas yang menjadi pusat perhatian
+          umat.
+        </p>
+
+        <p className="text-gray-700 leading-relaxed mb-4">
+          Dengan pengalaman dan dedikasi tinggi kami menghadirkan kubah yang
+          menggabungkan nilai estetika, kekuatan material, serta detail seni
+          Islami yang penuh makna. Proses pengerjaan dilakukan oleh tenaga ahli
+          berpengalaman, menggunakan material pilihan, serta teknologi modern,
+          sehingga setiap kubah yang kami hasilkan memiliki standar mutu tinggi,
+          tahan lama, dan bernilai keindahan tinggi.
+        </p>
+
+        <p className="text-gray-700 leading-relaxed mb-4">
+          Sejalan dengan slogan kami{" "}
+          <span className="font-semibold">
+            “SETIAP KUBAH YANG KAMI BANGUN, MENJADI AMANAH YANG KAMI JAGA”
+          </span>
+          , kami berkomitmen untuk menjaga kepercayaan setiap klien. Bagi kami,
+          membangun kubah bukan hanya soal konstruksi, tetapi juga tanggung
+          jawab moral dan spiritual.
+        </p>
+
+        <p className="text-gray-700 leading-relaxed">
+          Dome Indonesia hadir untuk memberikan karya terbaik, agar setiap kubah
+          menjadi warisan berharga yang menambah keindahan dan keberkahan rumah
+          ibadah di seluruh Indonesia.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
