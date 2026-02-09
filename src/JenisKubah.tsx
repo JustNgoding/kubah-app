5dd48f40d558671512f659b80da2333badfcb7bc
PitoyoAidropHunter <pitoyoaidrophunter@gmail.com>
2026-02-09 13:10:44 +0700

    add new pafe : jenis-kubah

---
+++ src/JenisKubah.tsx
@@ -0,0 +1,16 @@
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+const JenisKubah = () => {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <h1>Jenis Kubah Page</h1>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default JenisKubah;
