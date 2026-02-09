0c9b9574b2c82191037f0ba23e543d41213a92e4
PitoyoAidropHunter <pitoyoaidrophunter@gmail.com>
2026-02-09 13:12:33 +0700

    add new route : jenis-kubah

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
+import JenisKubah from "./JenisKubah.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/jenis-kubah" element={<JenisKubah />} />
     </Routes>
   </BrowserRouter>,
 );
